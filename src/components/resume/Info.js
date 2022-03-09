d1807bd813c3f3085f8f080fce41fe61b2f92890
danielprudhomme <daniel.prudhomme.pro@gmail.com>
2022-03-09 13:48:37 +0100

    Address in info

--- src/components/resume/Info.js
+++ src/components/resume/Info.js
@@ -3,10 +3,16 @@ import ColumnContainer from '../layout/ColumnContainer';
 import './Info.scss';
 
 function Info({ data }) {
+  let address = null;
+  if (data.address) {
+    address = <div className='Address'>{data.address.numberAndStreet} - {data.address.zipCode} {data.address.city}, <span className='Country'>{data.address.country}</span></div>
+  }
+
   const left =
     <div className='Info-left'>
       <p>{data.mail}</p>
       <p>{data.phone}</p>
+      {address}
     </div>;
 
   const right =
