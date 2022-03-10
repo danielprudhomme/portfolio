ab0c7a87853453b2b8936932a6689635975e185b
danielprudhomme <daniel.prudhomme.pro@gmail.com>
2022-03-10 21:42:05 +0100

    Map icon

--- src/components/resume/ExperienceDetails.js
+++ src/components/resume/ExperienceDetails.js
@@ -1,3 +1,4 @@
+import mapIcon from '../../images/icons/map.png';
 import ColumnContainer from '../layout/ColumnContainer';
 import './ExperienceDetails.scss';
 import StartEndDate from './StartEndDate';
@@ -5,8 +6,10 @@ import StartEndDate from './StartEndDate';
 function ExperienceDetails({ data }) {
   const left = <div>
     <div className='ExperienceLeft'>
-      <div className='Company'>{data.company}</div>
+      <div className='Company'>{data.company}<i class="bi-alarm"></i></div>
+      
       <div className='Location'>
+      <img className='LocationIcon' src={mapIcon} alt="Location" />
         <div className='City'>{data.location.city}</div>
         {data.location.country ? <div className='Country'>, {data.location.country}</div> : null}
       </div>
